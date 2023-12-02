12f440f817fbe254f19592e1b9cd1cb04b5d9f7e
Renan Santos <renansantos7@proton.me>
2023-12-02 13:01:59 -0300

    1

--- js/componentes/pesquisa-artigo.js
+++ js/componentes/pesquisa-artigo.js
@@ -1,7 +1,7 @@
 export default function pesquisaArt() {
     const divPesquisaArt = document.querySelector('#pesquisa-artigo')
     divPesquisaArt.innerHTML = `
-        
+        <span id="botao-pesquisa-abrir" class="material-symbols-outlined botoes-laterais">search</span>
         <section id="procura-artigo">
             <span id="botao-pesquisa-fechar" class="material-symbols-outlined menu-fechar">close</span>
             
@@ -16,19 +16,3 @@ export default function pesquisaArt() {
         </section>
     `
 }
-
-/*
-
-<section id="procura-artigo">
-    <span id="botao-pesquisa-fechar" class="material-symbols-outlined menu-fechar">close</span>
-    
-    <h2>Pesquisar artigo</h2>
-    <label id="label-numero" for="input-numero-artigo">Digite o número do artigo</label>
-    <input id="input-numero-artigo" type="text" min="1" name="numero-artigo" />
-
-    <input id="botao-pesquisar-artigo" type="button" value="Pesquisar" />
-
-    <div class="linha-horizontal"></div>
-    <div id="resultados-pesquisa"></div>
-</section>
-*/
